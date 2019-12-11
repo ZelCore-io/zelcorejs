c87aecd2abd7cf56e114a68d1a8f1d93202e0b92
TheTrunk <tadeas.kmenta@zel.cash>
2019-12-11 14:50:10 +0100

    vbk - nodecore privatekey format vbk - correct transaction test

--- test/vbkaddress.js
+++ test/vbkaddress.js
@@ -16,3 +16,28 @@ it('VBK - isValidStandardAddress() recognises valid addresses', function () {
   expect(boolean).to.equal(true);
   expect(boolean2).to.equal(false);
 })
+
+it('VBK - import Nodecore privatekey', () => {
+  const privateNodecoreKey =
+    '40303E020100301006072A8648CE3D020106052B8104000A0427302502010104205E5356079422753D981D93BDEBF7DE38D4AB063A73540DB50F446D4F4618684C3056301006072A8648CE3D020106052B8104000A0342000461CA7B88C1B097CE7D72860C05D50AF154C63D112E149EE4A36F51E12D960C9065CAFE6CD9D11B54F03DB1A2A82A374E0357C9C63A2D6A1D1C9DF4C889910CAA';
+  const keyPair = zeltrezjs.veriblockcrypto.KeyPair.importFromNodecorePrivateKey(privateNodecoreKey);
+
+  expect(keyPair.publicKey.getAddress()).to.equal(
+    'V3kHRaCL6ddmqMRnCSyD8417Z9nJCZ'
+  );
+});
+
+it('VBK - export to Nodecore', () => {
+  // any valid private key
+  const privateNodecoreKey =
+    '40303E020100301006072A8648CE3D020106052B8104000A0427302502010104205E5356079422753D981D93BDEBF7DE38D4AB063A73540DB50F446D4F4618684C3056301006072A8648CE3D020106052B8104000A0342000461CA7B88C1B097CE7D72860C05D50AF154C63D112E149EE4A36F51E12D960C9065CAFE6CD9D11B54F03DB1A2A82A374E0357C9C63A2D6A1D1C9DF4C889910CAA';
+  const privHex =
+    '303e020100301006072a8648ce3d020106052b8104000a0427302502010104205e5356079422753d981d93bdebf7de38d4ab063a73540db50f446d4f4618684c';
+  const priv = zeltrezjs.veriblockcrypto.PrivateKey.fromStringHex(privHex);
+
+  const kp = zeltrezjs.veriblockcrypto.KeyPair.fromPrivateKey(priv);
+  const nodecoreFormat = kp.exportToNodecore();
+
+  expect(nodecoreFormat.address).to.equal('V3kHRaCL6ddmqMRnCSyD8417Z9nJCZ');
+  expect(nodecoreFormat.private_key).to.equal(privateNodecoreKey);
+});
--- test/vbktransaction.js
+++ test/vbktransaction.js
@@ -4,33 +4,32 @@ const BigNumber = require('bignumber.js');
 var expect = chai.expect
 
 it('VBK - Constructs and signs VBK standard transactions', function () {
-  const transaction = zeltrezjs.veriblocktransaction;
+  const transaction = zeltrezjs.veriblocktxs;
   const crypto = zeltrezjs.veriblockcrypto;
   const address = zeltrezjs.veriblockaddress;
-  // const basic = zeltrezjs.veriblockbasic;
-  // const PRIVATE_KEY = Buffer.from('303E020100301006072A8648CE3D020106052B8104000A04273025020101042017869E398A7ACD18729B8FC6D47DCFE9C1A2B5871334D00471EFC3985762FF8F', 'hex');
-  // const kp = crypto.KeyPair.fromPrivateKey(PRIVATE_KEY);
-  // const addr = address.Address.fromPublicKey(kp.publicKey);
-  // const outputs = [new basic.Output(addr, 100)];
-  // const tx = new transaction.Transaction(1, addr, 100, outputs, 1);
-  // const signatureIndex = 0;
-  // const signed = transaction.signTransaction(tx, kp, signatureIndex);
-  // expect(transaction.getTransactionId(signed.transaction, signatureIndex).toString('hex')).to.equal('4cb778a158601701c98028b778e583859ef814ba1a57284fadef720a1dd5fbb7');
-  // expect(signed.publicKey.asn1.toString('hex')).to.equal('3056301006072a8648ce3d020106052b8104000a034200044b649515a30a4361dd875f8fad16c37142116217e5b8069c444773b59911bcce38782d7ba06c0b9b771305d065279ce9f2288c8eab5328d260629085f7653504');
-  // expect(signed.publicKey.address).to.equal('V5ZguGxnAckADJMkFFG6Vpr9EGyk6v');
-  // expect(signed.signature.asn1.toString('hex')).to.equal('304502204348ceb1eaaf54cca7811aeae8590a3289840fc5f0a5ebb711c5a5e7f7fa0f329cb754428ceb80337f754313390ba52d2bb201eaa79c89b95603b8fd3d8f8f0670ecfa');
-
-  const constants = zeltrezjs.veriblockconstants;
   var PRIVATE_KEY = Buffer.from('303E020100301006072A8648CE3D020106052B8104000A04273025020101042017869E398A7ACD18729B8FC6D47DCFE9C1A2B5871334D00471EFC3985762FF8F', 'hex');
   var kp = crypto.KeyPair.fromPrivateKey(PRIVATE_KEY);
   var sourceAddress = address.addressFromPublicKey(kp.publicKey);
-  var tx = new transaction.Transaction(sourceAddress, constants.AMOUNT_MAX, [new transaction.Output(sourceAddress, new BigNumber(100))], 1);
-  console.log(tx);
-  var signatureIndex = 0;
-  var signed = transaction.signTransaction(tx, kp, signatureIndex);
+  const sourceAmount = (new BigNumber(10000000000)).toString();
+  const destAmount = (new BigNumber(10000000)).toString();
+  const recipientAddress = 'VALXnENuqFexnM9Nih6DVG6RfAy9jV';
+  const rawTx = {
+    sourceAddress,
+    sourceAmount,
+    data: "",
+    outputs: [
+      {
+        address: recipientAddress,
+        amount: destAmount,
+      },
+    ],
+  };
+  // deserialize rawTx
+  const modelTx = transaction.tryDeserializeTransaction(rawTx);
+  const signatureIndex = 0;
+  var signed = transaction.signTransaction(modelTx, kp, signatureIndex);
+  const txid = signed.transaction.txId;
 
-  var str = signed.stringify();
-  var deserialized = transaction.SignedTransaction.parse(str);
-  expect(deserialized.stringify()).to.equal(str);
-  expect(deserialized.stringify()).to.equal('{"signature":"30450221009c3d7cdb6bd7939f7acabd7e3b6eb00900f5cb496d6bd99354fe3f073b7b89d30220268c65be85c2b677459ccd5ababd7f764eb227b3e051dbe14a7f63b97a4865eb","publicKey":"3056301006072a8648ce3d020106052b8104000a034200044b649515a30a4361dd875f8fad16c37142116217e5b8069c444773b59911bcce38782d7ba06c0b9b771305d065279ce9f2288c8eab5328d260629085f7653504","signatureIndex":0,"transaction":{"type":1,"sourceAddress":"V5ZguGxnAckADJMkFFG6Vpr9EGyk6v","sourceAmount":9223372036854775807,"outputs":[{"address":"V5ZguGxnAckADJMkFFG6Vpr9EGyk6v","amount":100}],"networkByte":1}}');
+  expect(txid).to.equal('94230445fb9f83bcb4a2e9f043b030cd61e7ccf6c94bb48710d17b0719ed4aa0');
+  expect(JSON.stringify(signed)).to.equal('{"signature":{"_canonical":{"type":"Buffer","data":[111,45,70,64,195,191,123,72,221,213,180,112,152,202,95,100,60,99,238,131,37,32,167,231,213,127,211,225,202,14,182,159,40,211,39,84,77,149,145,15,122,20,95,218,56,176,197,81,5,222,111,59,104,187,193,55,129,80,227,251,140,98,69,201]}},"publicKey":{"_full":{"type":"Buffer","data":[48,86,48,16,6,7,42,134,72,206,61,2,1,6,5,43,129,4,0,10,3,66,0,4,75,100,149,21,163,10,67,97,221,135,95,143,173,22,195,113,66,17,98,23,229,184,6,156,68,71,115,181,153,17,188,206,56,120,45,123,160,108,11,155,119,19,5,208,101,39,156,233,242,40,140,142,171,83,40,210,96,98,144,133,247,101,53,4]}},"signatureIndex":0,"transaction":{"sourceAddress":"V5ZguGxnAckADJMkFFG6Vpr9EGyk6v","sourceAmount":"10000000000","data":"","outputs":[{"address":"VALXnENuqFexnM9Nih6DVG6RfAy9jV","amount":"10000000"}],"transactionFee":"9990000000","txId":"94230445fb9f83bcb4a2e9f043b030cd61e7ccf6c94bb48710d17b0719ed4aa0","type":1}}');
 })
